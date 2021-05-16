2d5970c562fcf56dc71a62086b8f21c8fb642d65
Daniel John <daniel.john.projects@gmail.com>
2021-05-16 14:02:32 +1000

    Basic setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cryptolib',
+    version='0.0.1',
+    description='Tools for solving cryptopals (and other) challenges.',
+    author='Daniel John',
+    author_email='daniel.john.projects@gmail.com',
+)
