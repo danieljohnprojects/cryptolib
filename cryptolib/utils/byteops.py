5622bdcf67c30aae09602bc754eada018297bd99
Daniel John <daniel.john.projects@gmail.com>
2021-05-16 16:11:23 +1000

    Added hamming distance function

--- cryptolib/utils/byteops.py
+++ cryptolib/utils/byteops.py
@@ -18,3 +18,20 @@ def cyclical_xor(key: bytes, message: bytes) -> bytes:
     r = len(message) % len(key)
     long_key = key * q + key[:r]
     return bytes([a^b for a,b in zip(long_key, message)])
+
+def hamming_distance(m1: bytes, m2:bytes) -> int:
+    """
+    Determines the hamming distance between two messages of bytes.
+
+    m1 and m2 should be the same length.
+    
+    Example:
+    >>> m1 = bytes(b'this is a test')
+    >>> m2 = bytes(b'wokka wokka!!!')
+    >>> hamming_distance(m1, m2)
+    37
+    """
+    if len(m1) != len(m2):
+        raise ValueError("Messages must be the same length.")
+    bits_different = cyclical_xor(m1, m2)
+    return bin(int.from_bytes(bits_different, 'big')).count('1')
