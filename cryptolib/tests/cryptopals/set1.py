49a3ed8fb53cf808be2e45cda5010be380379356
Daniel John <daniel.john.projects@gmail.com>
2021-05-19 15:34:19 +1000

    Challenge08 solution

--- cryptolib/tests/cryptopals/set1.py
+++ cryptolib/tests/cryptopals/set1.py
@@ -8,7 +8,7 @@ from cryptolib.utils.byteops import cyclical_xor
 from cryptolib.utils.conversion import hex_string_to_b64, b64_string_to_hex
 from cryptolib.utils.plain_scoring import score
 
-from .data import challenge04, challenge06, challenge07
+from .data import challenge04, challenge06, challenge07, challenge08
 
 import unittest
 
@@ -224,9 +224,22 @@ class Challenge08(Challenge):
 
     Remember that the problem with ECB is that it is stateless and deterministic; the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
     """
-    
+    ciphertexts = challenge08.ciphertexts
+
+    solution = 'd880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a'
+
     def __init__(self):
         self.name = "Challenge08"
+
+    def solve(self):
+        B = 16
+        ecb_enc = None
+        for ctext in self.ciphertexts:
+            N = len(ctext) // B
+            blocks = [ctext[B*i:B*(i+1)] for i in range(N)]
+            if len(blocks) != len(set(blocks)):
+                ecb_enc = ctext
+        return ecb_enc
     
 
 def test_all():
