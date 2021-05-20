56be815dd7d63b2da23b56fd26234883173036d3
Daniel John <daniel.john.projects@gmail.com>
2021-05-20 11:35:08 +1000

    Added pre and post solve functions
    For non deterministic challenges

--- cryptolib/tests/cryptopals/Challenge.py
+++ cryptolib/tests/cryptopals/Challenge.py
@@ -5,9 +5,21 @@ class Challenge():
 
     solution = None
 
+    def presolve(self):
+        pass
+
     def solve(self):
         return 1
 
+    def postsolve(self):
+        """
+        Sometimes solutions are non-deterministic so some work may need to be done to determine if the solution is correct.
+        """
+        pass
+
     def test_challenge(self):
-        assert(self.solution == self.solve())
+        self.presolve()
+        s = self.solve()
+        self.postsolve()
+        assert(self.solution == s)
         print(f"Passed test: {self.name}")
