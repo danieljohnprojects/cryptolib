6feffa1f6e6e480e52f8a322582f40f036aecf5c
Daniel John <daniel.john.projects@gmail.com>
2021-05-26 21:58:39 +1000

    New AdditionalPlaintextOracle class.
    More general class to replace the ECB_suffix_oracle class. Allows for
    addition of prefix and suffix and use of any block cipher mode during
    encryption.

---
+++ cryptolib/oracles/AdditionalPlaintextOracle.py
@@ -0,0 +1,22 @@
+import secrets
+
+from .BCOracle import BCOracle
+
+class AdditionalPlaintextOracle(BCOracle):
+    """
+    Takes in a message, prepends a secret prefix, appends a secret suffix, and encrypts it with a random but fixed key.
+    """
+    def __init__(self, 
+            secret_prefix: bytes=b'', 
+            secret_suffix: bytes=b'',
+            mode: str = "ECB",
+            algorithm: str = "AES",
+            padding: str = "pkcs7",
+            key: bytes = None,
+            IV: bytes = None):
+        super().__init__(mode, algorithm, padding, key=key, IV=IV)
+        self.__secret_prefix = secret_prefix
+        self.__secret_suffix = secret_suffix
+
+    def __preprocess(self, message: bytes) -> bytes:
+        return self.__secret_prefix + message + self.__secret_suffix
