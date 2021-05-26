4a94222b12e04f71fe4f15e9b3c212e0b889cb85
Daniel John <daniel.john.projects@gmail.com>
2021-05-26 22:33:24 +1000

    Improved length finding and suffix decoding attack

--- cryptolib/cracks/bc_oracles.py
+++ cryptolib/cracks/bc_oracles.py
@@ -4,8 +4,10 @@ Attacks on block cipher oracles.
 
 from functools import reduce
 from math import gcd
+from typing import Tuple
 
-from ..oracles import BCOracle, ECB_suffix_oracle
+from ..oracles import AdditionalPlaintextOracle, BCOracle
+from ..utils.byteops import bytes_to_blocks
 
 def get_block_size(oracle: BCOracle, max_size: int = 20):
     """
@@ -53,30 +55,81 @@ def uses_ECB(oracle: BCOracle, block_size: int = 16) -> bool:
     # If any blocks repeat set(blocks) will have less elements than blocks
     return (len(blocks) != len(set(blocks)))
 
-def get_additional_message_len(oracle: BCOracle) -> int:
+def get_additional_message_len(
+        oracle: AdditionalPlaintextOracle, 
+        block_size: int
+        ) -> Tuple[int,int]:
     """
-    Determines the length of the plaintext being added to a message before encryption is performed in a block cipher oracle.
+    Determines the length of any prefix and suffix added to a message before being encrypted by an block cipher oracle that uses a fixed IV.
 
-    Does this by comparing the length of an encryption of the empty string with the encryption of strings of varying length. 
+    The method for determining the prefix length is easiest explained with an example:
+
+    Suppose we have an oracle that takes a message, prepends 'A' to it, appends 'B' to it, pads the altered message, and finally encrypts it with a block size of 4. So given an empty string the oracle returns the encryption of the string "AB\x02\x02".
+    The following messages are sent, processed and received back in pairs:
+    a    -> AaB.|     -> ****
+    b    -> AbB.|     -> ****
+    aa   -> AaaB|.... -> ********
+    ab   -> AabB|.... -> ********
+    aaa  -> Aaaa|B... -> ********
+    aab  -> Aaab|B... -> ********
+    aaaa -> Aaaa|aB.. -> yyyy****
+    aaab -> Aaaa|bB.. -> yyyy****
+    We see that in the first three pairs of messages the ciphers returned by the oracle all differ in the first block. In the final pair the messages the first block of cipher is the same.
+    We thus need to send pairs of messages until we see a change in the index of the first block that differs.
+
+    The total length of the additional plaintext is easily determined by keeping track of when the ciphertext length jumps.
+
+    The suffix length is just the difference of the total length and the prefix length.
     """
-    message = b''
-    init_len = len(oracle.divine(message))
-    while (len(oracle.divine(message)) == init_len):
-        message += b'a'
-    return init_len - len(message)
+    
+    indexes = []
+    lengths = []
+    for i in range(block_size):
+        # Send two messages that differ in the ith byte.
+        zero_enc = oracle.divine(b'\x00' * i + b'\x00')
+        one_enc  = oracle.divine(b'\x00' * i + b'\xff')
 
-def decode_suffix(oracle: ECB_suffix_oracle, block_size: int = 16) -> bytes:
+        # Keep track of lengths of replies.
+        lengths.append(len(zero_enc))
+
+        # Determine the first block in which the replies differ.
+        zero_blocks = bytes_to_blocks(zero_enc, block_size)
+        one_blocks = bytes_to_blocks(one_enc, block_size)
+        same_blocks = [x == y for x, y in zip(zero_blocks, one_blocks)]
+        try:
+            message_block_idx = same_blocks.index(False)
+        except ValueError:
+            raise ValueError("Oracle does not work as expected! Encrypts different messages to same output.")
+        indexes.append(message_block_idx)
+    
+    # Determine total length of added plaintext.
+    total_len = lengths[0] - (lengths.count(lengths[0]) + 1)
+
+    # Determine length of prefix
+    remainder = block_size - indexes.count(indexes[0])
+    prefix_len = (indexes[0] * block_size) + remainder
+
+    suffix_len = total_len - prefix_len
+
+    return prefix_len, suffix_len
+
+def decode_suffix(
+    oracle: AdditionalPlaintextOracle, 
+    suffix_len: int,
+    block_size: int = 16,
+    ) -> bytes:
     """
     Decodes the suffix used in an ECB_suffix_oracle object.
     """
-    suffix_len = get_additional_message_len(oracle)
-    # The number of blocks needed to fit in the message
+
+    # The maximum number of blocks needed to fit the suffix
     num_blocks = suffix_len // block_size + 1
     message = b'\x00' * block_size * num_blocks
     suffix = b''
     for _ in range(suffix_len):
         # Pop off the first character
         message = message[1:]
+        # First character of 
         target = oracle.divine(message)
         for i in range(256):
             trial = oracle.divine(message + suffix + bytes([i]))
@@ -84,5 +137,4 @@ def decode_suffix(oracle: ECB_suffix_oracle, block_size: int = 16) -> bytes:
                 # We have found the next byte of the suffix!
                 suffix += bytes([i])
                 break
-                
     return suffix
