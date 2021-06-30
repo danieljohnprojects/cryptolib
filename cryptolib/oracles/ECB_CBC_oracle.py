b563117fdfe22be812f751db3c85440e650177d9
Daniel John <daniel.john.projects@gmail.com>
2021-06-30 22:59:50 +1000

    Updated for pipline structure

--- cryptolib/oracles/ECB_CBC_oracle.py
+++ cryptolib/oracles/ECB_CBC_oracle.py
@@ -2,34 +2,47 @@ from cryptolib.utils.byteops import bytes_to_blocks
 from cryptolib.utils.padding import Padder
 import secrets
 
-from ..blockciphers import CBCMode, ECBMode
+# from ..blockciphers import CBCMode, ECBMode
 from .Oracle import Oracle
+
+from ..pipes import BCEncryptPipe, PadPKCS7Pipe
 
 class ECB_CBC_oracle(Oracle):
     """
     Takes in a message of bytes, alters it and then encrypts with either ECB or CBC under a random key.
     """
     def __init__(self):
-        self._cbc = lambda : CBCMode(
+        self.pipeline = [
+            self.add_prefix,
+            self.add_suffix,
+            PadPKCS7Pipe(),
+            self.choose_engine_and_encrypt
+        ]
+
+        self._cbc = lambda : BCEncryptPipe(
+            'cbc',
             'AES', 
             secrets.token_bytes(16), 
-            IV=secrets.token_bytes(16), 
+            iv=secrets.token_bytes(16), 
             )
-        self._ecb = lambda : ECBMode(
+        self._ecb = lambda : BCEncryptPipe(
+            'ecb',
             'AES', 
             secrets.token_bytes(16), 
             )
         # Need a way to test our predictions of the oracle.
         self._last_choice = None
-        self.padder = Padder('pkcs7', 16)
 
-
-    def divine(self, message: bytes) -> bytes:
-        # Create a new cipher and discard it each call so that the key cannot be found
-        cipher, self._last_choice = secrets.choice([(self._cbc(), 'CBC'), (self._ecb(), 'ECB')])
+    @staticmethod
+    def add_prefix(message: bytes) -> bytes:
         prefix = secrets.token_bytes(secrets.choice(range(5, 11)))
+        return prefix + message
+
+    @staticmethod
+    def add_suffix(message: bytes) -> bytes:
         suffix = secrets.token_bytes(secrets.choice(range(5, 11)))
-        plain_blocks = bytes_to_blocks(prefix + message + suffix, cipher.block_size)
-        cipher_blocks = cipher.encrypt(plain_blocks)
-        ciphertext = b''.join(cipher_blocks)
-        return ciphertext
+        return message + suffix
+
+    def choose_engine_and_encrypt(self, message: bytes) -> bytes:
+        cipher, self._last_choice = secrets.choice([(self._cbc(), 'CBC'), (self._ecb(), 'ECB')])
+        return cipher(message)
