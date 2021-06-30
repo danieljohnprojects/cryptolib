544d07dd29661d8c666a5d55b8578ba05308d98c
Daniel John <daniel.john.projects@gmail.com>
2021-06-30 23:02:44 +1000

    Updated with pipes

--- cryptolib/oracles/AdditionalPlaintextOracle.py
+++ cryptolib/oracles/AdditionalPlaintextOracle.py
@@ -1,22 +1,18 @@
 from typing import Optional
 
-from .BCOracle import BCOracle
+from .Oracle import Oracle
+from ..pipes import BCEncryptPipe, PadPKCS7Pipe
 
-class AdditionalPlaintextOracle(BCOracle):
+class AdditionalPlaintextOracle(Oracle):
     """
-    Takes in a message, prepends a secret prefix, appends a secret suffix, and encrypts it with a random but fixed key in ECB mode.
+    Takes in a message, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a random but fixed key in ECB mode.
     """
     def __init__(self, 
             secret_prefix: bytes=b'', 
             secret_suffix: bytes=b'',
-            mode: str = "ECB",
-            algorithm: str = "AES",
-            padding: str = "pkcs7",
-            key: Optional[bytes] = None,
-            IV: Optional[bytes] = None):
-        super().__init__(mode, algorithm, padding, key=key, IV=IV)
-        self._secret_prefix = secret_prefix
-        self._secret_suffix = secret_suffix
-
-    def _preprocess(self, message: bytes) -> bytes:
-        return self._secret_prefix + message + self._secret_suffix
+            algorithm: str = "AES",):
+        self.pipeline = [
+            lambda message: secret_prefix + message + secret_suffix,
+            PadPKCS7Pipe(),
+            BCEncryptPipe('ecb', algorithm)
+        ]
