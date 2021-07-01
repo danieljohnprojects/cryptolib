6c3bb381a532ca9485fe7a5d5c9c51c9471132e8
Daniel John <daniel.john.projects@gmail.com>
2021-07-01 14:30:55 +1000

    Added ability to specify mode.

--- cryptolib/oracles/AdditionalPlaintextWithQuotingOracle.py
+++ cryptolib/oracles/AdditionalPlaintextWithQuotingOracle.py
@@ -5,16 +5,18 @@ from ..pipes import BCEncryptPipe, PadPKCS7Pipe
 
 class AdditionalPlaintextWithQuotingOracle(Oracle):
     """
-    Takes in a message, quotes out specific characters, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key in ECB mode.
+    Takes in a message, quotes out specific characters, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key and iv in the specified mode (defaults to ECB).
 
-    If no key is provided one is randomly generated.
+    If no key (iv) is provided one is randomly generated (if not in ECB mode).
     """
     def __init__(self,
             secret_prefix: bytes=b'', 
             secret_suffix: bytes=b'',
             quote_chars: bytes=b'',
+            mode: str = 'ecb',
             algorithm: str = "AES",
-            key: Optional[bytes] = None
+            key: Optional[bytes] = None,
+            iv: Optional[bytes] = None
             ):
         self.pipeline = []
         for char in quote_chars:
@@ -26,5 +28,5 @@ class AdditionalPlaintextWithQuotingOracle(Oracle):
         self.pipeline += [
             lambda message: secret_prefix + message + secret_suffix,
             PadPKCS7Pipe(),
-            BCEncryptPipe('ecb', algorithm, key)
+            BCEncryptPipe(mode, algorithm, key, iv)
         ]
