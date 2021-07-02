7aa23342eb3116b2752b476aeef362296e64e42f
Daniel John <daniel.john.projects@gmail.com>
2021-07-02 17:29:57 +1000

    Changed structure of AddPT models

--- cryptolib/oracles/AdditionalPlaintextOracle.py
+++ cryptolib/oracles/AdditionalPlaintextOracle.py
@@ -1,14 +1,18 @@
+from cryptolib.pipes.GenIV import GenIV
 from typing import Optional
 
+from .Oracle import Oracle
 from .SequentialOracle import SequentialOracle
-from ..pipes import BCEncrypt, PadPKCS7
+from ..pipes import ECBEncrypt, CBCEncrypt, PadPKCS7, GenIV
 
 
 class AdditionalPlaintextOracle(SequentialOracle):
     """
-    Takes in a message, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key in ECB mode.
+    Takes in a message, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key in the specified mode.
 
     If no key is provided one is randomly generated.
+
+    If the specified mode uses IVs, a new one is generated for each message.
     """
 
     def __init__(self,
@@ -16,16 +20,23 @@ class AdditionalPlaintextOracle(SequentialOracle):
                  secret_suffix: bytes = b'',
                  mode: str = 'ecb',
                  algorithm: str = "AES",
-                 key: Optional[bytes] = None,
-                 iv: Optional[bytes] = None
-                 ):
-        self.pipeline = [
-            lambda message: secret_prefix + message + secret_suffix,
-            PadPKCS7(),
-            BCEncrypt(
-                mode=mode,
-                algorithm=algorithm,
-                key=key,
-                iv=iv
-            )
-        ]
+                 key: Optional[bytes] = None):
+        if mode == 'ecb':
+            pipeline = [
+                Oracle(lambda message: secret_prefix + message + secret_suffix),
+                PadPKCS7(),
+                ECBEncrypt(algorithm, key)
+            ]
+        elif mode == 'cbc':
+            engine = CBCEncrypt(algorithm, key)
+            pipeline = [
+                GenIV(engine.block_size),
+                Oracle(lambda message: secret_prefix + message + secret_suffix),
+                PadPKCS7(),
+                engine
+            ]
+        else:
+            raise ValueError(f"Mode {mode} is not supported.")
+
+        super().__init__(pipeline)
+
--- cryptolib/oracles/AdditionalPlaintextWithQuotingOracle.py
+++ cryptolib/oracles/AdditionalPlaintextWithQuotingOracle.py
@@ -1,14 +1,16 @@
 from typing import Optional
 
-from .SequentialOracle import SequentialOracle
-from ..pipes import BCEncrypt, PadPKCS7
+from .SequentialOracle import Oracle, SequentialOracle
+from ..pipes import ECBEncrypt, CBCEncrypt, PadPKCS7, GenIV
 
 
 class AdditionalPlaintextWithQuotingOracle(SequentialOracle):
     """
-    Takes in a message, quotes out specific characters, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key and iv in the specified mode (defaults to ECB).
+    Takes in a message, quotes out specific characters, prepends a secret prefix, appends a secret suffix, pads it, and encrypts it with a fixed key in the specified mode.
 
-    If no key (iv) is provided one is randomly generated (if not in ECB mode).
+    If no key is provided one is randomly generated.
+
+    If the specified mode uses IVs, a new one is generated for each message.
     """
 
     def __init__(self,
@@ -17,18 +19,30 @@ class AdditionalPlaintextWithQuotingOracle(SequentialOracle):
                  quote_chars: bytes = b'',
                  mode: str = 'ecb',
                  algorithm: str = "AES",
-                 key: Optional[bytes] = None,
-                 iv: Optional[bytes] = None
+                 key: Optional[bytes] = None
                  ):
-        self.pipeline = []
+        pipeline = []
         for char in quote_chars:
             b = bytes(char)
-            self.pipeline.append(
-                lambda message: message.replace(b, b'"' + b + b'"')
+            pipeline.append(
+                Oracle(lambda message: message.replace(b, b'"' + b + b'"'))
             )
 
-        self.pipeline += [
-            lambda message: secret_prefix + message + secret_suffix,
-            PadPKCS7(),
-            BCEncrypt(mode, algorithm, key, iv)
-        ]
+        if mode == 'ecb':
+            pipeline += [
+                Oracle(lambda message: secret_prefix + message + secret_suffix),
+                PadPKCS7(),
+                ECBEncrypt(algorithm, key)
+            ]
+        elif mode == 'cbc':
+            engine = CBCEncrypt(algorithm, key)
+            pipeline += [
+                GenIV(engine.block_size),
+                Oracle(lambda message: secret_prefix + message + secret_suffix),
+                PadPKCS7(),
+                engine
+            ]
+        else:
+            raise ValueError(f"Mode {mode} is not supported.")
+
+        super().__init__(pipeline)
