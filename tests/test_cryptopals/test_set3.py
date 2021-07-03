c9ab623709f414c6a9ec9b4ce74240b0eb104e96
Daniel John <daniel.john.projects@gmail.com>
2021-07-03 19:04:58 +1000

    Challenge18 solution

--- tests/test_cryptopals/test_set3.py
+++ tests/test_cryptopals/test_set3.py
@@ -1,6 +1,9 @@
 import pytest
 
+import base64
+
 from cryptolib.cracks.bc_oracles import decrypt_with_padding_oracle
+from cryptolib.pipes import CTR
 from cryptolib.utils.padding import strip_pkcs7
 from .data import challenge17
 
@@ -54,4 +57,48 @@ def test_Challenge17():
         cipher = client(message)
         decrypted = strip_pkcs7(decrypt_with_padding_oracle(cipher, server, 'cbc'), 16)
         assert decrypted == message
-        
+
+def test_Challenge18():
+    """
+    The string:
+    L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==
+    ... decrypts to something approximating English in CTR mode, which is an AES block cipher mode that turns AES into a stream cipher, with the following parameters:
+
+        key=YELLOW SUBMARINE
+        nonce=0
+        format=64 bit unsigned little endian nonce,
+                64 bit little endian block count (byte count / 16)
+
+    CTR mode is very simple.
+
+    Instead of encrypting the plaintext, CTR mode encrypts a running counter, producing a 16 byte block of keystream, which is XOR'd against the plaintext.
+
+    For instance, for the first 16 bytes of a message with these parameters:
+
+    keystream = AES("YELLOW SUBMARINE",
+                    "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+
+    ... for the next 16 bytes:
+
+    keystream = AES("YELLOW SUBMARINE",
+                    "\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00")
+
+    ... and then:
+
+    keystream = AES("YELLOW SUBMARINE",
+                    "\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00")
+
+    CTR mode does not require padding; when you run out of plaintext, you just stop XOR'ing keystream and stop generating keystream.
+
+    Decryption is identical to encryption. Generate the same keystream, XOR, and recover the plaintext.
+
+    Decrypt the string at the top of this function, then use your CTR function to encrypt and decrypt other things. 
+    """
+    message = base64.b64decode(b'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+
+    key = b'YELLOW SUBMARINE'
+    oracle =  CTR('aes', key)
+    nonce = bytes(8)
+    decrypted = oracle(nonce + message)
+
+    assert decrypted == b"Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
