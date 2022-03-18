07d7904a9826cb3b764204379112141106bef361
Daniel John <daniel.john.projects@gmail.com>
2022-03-18 12:00:46 +1100

    Moved to md4 directory

--- lib/MDHashes/src/md4/MD4.h
+++ lib/MDHashes/src/md4/MD4.h
@@ -4,7 +4,13 @@
 
 #define DIGEST_LENGTH 16
 
-#include "constants_global.h"
+// Padding is added to a message so that its length in bytes is congruent to 
+// PAD_REMAINDER modulo PAD_BLOCK.
+#define PAD_BLOCK 64
+#define PAD_REMAINDER 56
+#define WORDS_PER_BLOCK 16
+// ROTATE_LEFT rotates the word x n bits to the left.
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
 
 // Functions and constants for Round 1:
 #define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
