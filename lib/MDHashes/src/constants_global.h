045f353ee41b6d136b62f146c52f502b342c4c3c
Daniel John <daniel.john.projects@gmail.com>
2022-03-19 16:39:30 +1100

    Removed useless files

--- lib/MDHashes/src/constants_global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Padding is added to a message so that its length in bytes is congruent to 
-// PAD_REMAINDER modulo PAD_BLOCK.
-#define PAD_BLOCK 64
-#define PAD_REMAINDER 56
-#define WORDS_PER_BLOCK 16
-// ROTATE_LEFT rotates the word x n bits to the left.
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
