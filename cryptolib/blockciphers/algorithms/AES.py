a28c691a331bb275bb00fb07e10c07c547c14cda
Daniel John <daniel.john.projects@gmail.com>
2022-03-26 09:42:07 +1100

    Moved c-libraries to class attributes

--- cryptolib/blockciphers/algorithms/AES.py
+++ cryptolib/blockciphers/algorithms/AES.py
@@ -14,27 +14,32 @@ class AES(Engine):
     """
 
     block_size = 16
+    libpath128 = build_filename('build/AES/libaes128.so')
+    libpath192 = build_filename('build/AES/libaes192.so')
+    libpath256 = build_filename('build/AES/libaes256.so')
+    AES128libC = CDLL(libpath128)
+    AES192libC = CDLL(libpath192)
+    AES256libC = CDLL(libpath256)
 
     def __init__(self, key: bytes):
         # Key can be either 128, 192, or 256 bits long.
         if len(key) == 16:
-            libpath = build_filename('build/AES/libaes128.so')
             key_schedule_len = 16 * 11  # 10 rounds plus initial key of 16 bytes
+            self._libC = self.AES128libC
         elif len(key) == 24:
-            libpath = build_filename('build/AES/libaes192.so')
             key_schedule_len = 16 * 13  # 12 rounds plus initial key of 16 bytes
+            self._libC = self.AES192libC
         elif len(key) == 32:
-            libpath = build_filename('build/AES/libaes256.so')
             key_schedule_len = 16 * 15  # 14 rounds plus initial key of 16 bytes
+            self._libC = self.AES256libC
         else:
             raise ValueError(
                 f"Key must be 16, 24, or 32 bytes long, got {len(key)}.")
 
         # Load library functions
-        self._AESlibC = CDLL(libpath)
         # Initialise the key schedule
         self._key_schedule = create_string_buffer(key_schedule_len)
-        self._AESlibC.initialise_key(key, self._key_schedule)
+        self._libC.initialise_key(key, self._key_schedule)
 
     def encrypt(self, message: bytes) -> bytes:
         if (len(message) != self.block_size):
@@ -42,7 +47,7 @@ class AES(Engine):
                 f"Message length must be f{self.block_size} bytes. Got {len(message)}.")
 
         ciphertext = create_string_buffer(self.block_size)
-        self._AESlibC.encrypt(self._key_schedule, message, ciphertext)
+        self._libC.encrypt(self._key_schedule, message, ciphertext)
         return ciphertext.raw
 
     def decrypt(self, ciphertext: bytes) -> bytes:
@@ -51,5 +56,5 @@ class AES(Engine):
                 f"Ciphertext length must be f{self.block_size} bytes. Got {len(ciphertext)}.")
 
         plain = create_string_buffer(self.block_size)
-        self._AESlibC.decrypt(self._key_schedule, ciphertext, plain)
+        self._libC.decrypt(self._key_schedule, ciphertext, plain)
         return plain.raw
