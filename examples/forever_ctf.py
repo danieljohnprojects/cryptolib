86add3cc44e9135be53ded661e0ff17ae3edbd54
Daniel John <daniel.john.projects@gmail.com>
2022-03-29 16:59:23 +1100

    Updated to new structure

--- examples/forever_ctf.py
+++ examples/forever_ctf.py
@@ -3,35 +3,34 @@ import string
 
 from pwn import *
 
-from cryptolib.pipes import Pipe
-from cryptolib.cracks.bc_oracles import uses_ECB, get_block_size, get_additional_message_len, decode_suffix
+from cryptolib.blockciphers.chosen_plain.attacks import get_block_size, diagnose_mode, get_additional_message_len, decrypt_suffix
 
-class ForeverCTF_AES_Oracle(Pipe):
-    def __init__(self, **kwargs):
+class ForeverCTF_AES_Oracle:
+    def __init__(self):
         host = 'forever.isss.io'
         port = 3103
-        conn = remote(host, port)
-        kwargs['conn'] = conn
-        super().__init__(**kwargs)
+        self.conn = remote(host, port)
     
     def __call__(self, message: bytes) -> bytes:
-        conn = self.state['conn']
-        l = conn.recvline() # Send me a message >:)
-        conn.sendline(message)
-        l = conn.recvline() # Here's your encrypted message with the flag! Too bad you can't read it now >:D
-        return codecs.decode(conn.recvline(keepends=False), 'hex_codec')
+        l = self.conn.recvline() # Send me a message >:)
+        self.conn.sendline(message)
+        l = self.conn.recvline() # Here's your encrypted message with the flag! Too bad you can't read it now >:D
+        return codecs.decode(self.conn.recvline(keepends=False), 'hex_codec')
 
 oracle = ForeverCTF_AES_Oracle()
 allowable_bytes = bytes(string.printable[:-5], encoding='ascii')
-block_size = get_block_size(oracle, allowable_bytes=allowable_bytes)
-print(f"Block size: {block_size}") # 16
-print(f"ECB mode: {uses_ECB(oracle, block_size=block_size, allowable_bytes=allowable_bytes)}") # True
+print("Block size: ", end='')
+B = get_block_size(oracle, allowable_bytes=allowable_bytes)
+print(B) # 16
+print(f"Block cipher mode: ", end='')
+mode = diagnose_mode(oracle, block_size=B, allowable_bytes=allowable_bytes)
+print(mode) # ecb
 
-prefix_len, suffix_len = get_additional_message_len(oracle, block_size, allowable_bytes=allowable_bytes)
+prefix_len, suffix_len = get_additional_message_len(oracle, B, allowable_bytes=allowable_bytes)
 print(f"Prefix length: {prefix_len}") # 0
 print(f"Suffix length: {suffix_len}") # 41
 
-suffix = decode_suffix(oracle, suffix_len=suffix_len, prefix_len=prefix_len, block_size=block_size, allowable_bytes=allowable_bytes)
+suffix = decrypt_suffix(oracle, suffix_len=suffix_len, prefix_len=prefix_len, block_size=B, allowable_bytes=allowable_bytes)
 print(f"Suffix: {suffix}") # utflag{0nly_5ecur3_1f_y0u_us3_1t_pr0p3r1y
 
-oracle.state['conn'].close()
+oracle.conn.close()
