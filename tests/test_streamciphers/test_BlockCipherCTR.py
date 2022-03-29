7a5b11c6bb651caa4929da07daa4a89160c64ca8
Daniel John <daniel.john.projects@gmail.com>
2022-03-29 12:34:56 +1100

    Added test against reference cipher.

--- tests/test_streamciphers/test_BlockCipherCTR.py
+++ tests/test_streamciphers/test_BlockCipherCTR.py
@@ -1,5 +1,6 @@
 import pytest
 import random
+from Crypto.Cipher import AES
 from cryptolib.streamciphers.algorithms import BlockCipherCTR
 
 def test_BlockCipherCTR():
@@ -32,3 +33,16 @@ def test_BlockCipherCTR():
     # Check that encrypt and decrypt are inverses
     message = rng.randbytes(123)
     assert message == cipher.decrypt(cipher.encrypt(message))
+
+    # Check against pycryptodome implementation
+    nonce_size = 8
+    key_lens = [16] * 10 + [24] * 10 + [32] * 10
+    for key_len in key_lens:
+        key = rng.randbytes(key_len)
+        cipher = BlockCipherCTR('aes', key, nonce_size)
+        message = rng.randbytes(rng.randint(10, 200))
+        enc_message = cipher.encrypt(message)
+
+        reference_cipher = AES.new(key, AES.MODE_CTR, nonce=enc_message[:nonce_size])
+        expected_out = reference_cipher.encrypt(message)
+        assert enc_message[nonce_size:] == expected_out
