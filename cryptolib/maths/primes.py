52ff6a5967ec4143e37e52e87a7278f2f8763af3
Daniel John <daniel.john.projects@gmail.com>
2022-06-09 14:48:10 +1000

    Updated default options to be more robust

--- cryptolib/maths/primes.py
+++ cryptolib/maths/primes.py
@@ -4,7 +4,7 @@ Functions for generating and testing primes.
 import random
 from typing import Optional
 
-def fermat_test(p: int, samples: int = 10, seed: Optional[int] = None) -> bool:
+def fermat_test(p: int, samples: int = 100, seed: Optional[int] = None) -> bool:
     """
     Fermat's theorem states that if p is prime then a^(p-1) == 1 (mod p) for every a between 2 and p-1.
     In this function we test a random sample of values for a in the range [2,p-1], if all of these are congruent to 1 we return True otherwise we return False.
@@ -16,7 +16,7 @@ def fermat_test(p: int, samples: int = 10, seed: Optional[int] = None) -> bool:
     Returns:
         True if all a^(p-1) = 1 (mod p), False otherwise.
     """
-    if p < 100:
+    if p < 1000:
         A = [pow(a, p-1, p) == 1 for a in range(1, p)]
         return all(A)
     rng = random.Random(seed)
