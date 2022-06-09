28c5be6d549c3097ac90cfc8167672177ebdaaee
Daniel John <daniel.john.projects@gmail.com>
2022-06-09 14:47:47 +1000

    Fix to work with large p

--- cryptolib/maths/primes.py
+++ cryptolib/maths/primes.py
@@ -20,6 +20,7 @@ def fermat_test(p: int, samples: int = 10, seed: Optional[int] = None) -> bool:
         A = [pow(a, p-1, p) == 1 for a in range(1, p)]
         return all(A)
     rng = random.Random(seed)
-    A = rng.sample(range(2, p), samples)
+    # A = rng.sample(range(2, p), samples) # This doesn't work for very large p
+    A = [rng.randint(2, p) for _ in range(samples)]
     A = [pow(a, p-1, p) == 1 for a in A]
     return all(A)
