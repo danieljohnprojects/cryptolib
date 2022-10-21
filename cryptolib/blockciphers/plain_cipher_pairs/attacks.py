dd2c4d74f185bf021e419bad596c4cec229dbcf4
Daniel John <daniel.john.projects@gmail.com>
2022-10-21 21:07:33 +1100

    Added splice attack

--- cryptolib/blockciphers/plain_cipher_pairs/attacks.py
+++ cryptolib/blockciphers/plain_cipher_pairs/attacks.py
@@ -38,3 +38,46 @@ def exhaust_key(
             return test_key
     else:
         raise RuntimeError("No key found that produces a matching plaintext-ciphertext pair.")
+
+def splice_ECB_ciphertext(
+        plain_cipher_pairs: ((bytes, bytes), (bytes, bytes)),
+        block_size: int,
+    ) -> (bytes, bytes):
+    """
+    Given a collection of plaintext-ciphertext pairs originating from a block cipher in ECB mode we can construct a new pair by mixing the blocks of different messages together. 
+    
+    For example suppose the plaintexts:
+       - "Please send: $15 to Alice."
+       - "Please send: $0.05 to Bob."
+    were encrypted with AES in ECB mode, and that we have the corresponding ciphertexts. We could then splice these together to get the encryption of:
+         "Please send: $1505 to Bob."
+    
+    This function is likely not going to be particularly useful but hopefully it sparks your imagination if a similar opportunity present itself.
+    
+    Usage:
+        import secrets
+        from cryptolib.blockciphers.chosen_plain.oracles import EncryptECB
+        key = secrets.token_bytes(16)
+        enc = EncryptECB('aes', key)
+        dec = DecryptECB('aes', key)
+        p1 = b'Please send: $15 to Alice.'
+        p2 = b'Please send: $0.05 to Bob.'
+        c1 = enc(p1)
+        c2 = enc(p2)
+        p3, c3 = splice_ECB_ciphertext( ((p1, c1), (p2, c2)), 16 )
+        assert enc(p3) == c3
+        
+    Args:
+        plain_cipher_pairs: A tuple containing two plaintext-ciphertext pairs.
+        block_size: The block size of the underlying cipher.
+    Returns:
+        A new plaintext-ciphertext pair spliced from the old ones.
+    """
+    p1 = plain_cipher_pairs[0][0]
+    c1 = plain_cipher_pairs[0][1]
+    p2 = plain_cipher_pairs[1][0]
+    c2 = plain_cipher_pairs[1][1]
+    
+    splicedP = p1[:block_size] + p2[block_size:]
+    splicedC = c1[:block_size] + c2[block_size:]
+    return splicedP, splicedC
