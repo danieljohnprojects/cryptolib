ac1761a9aac49371792850dc8e59989f6ea1f93b
Daniel John <daniel.john.projects@gmail.com>
2022-10-22 21:42:32 +1100

    Added print_words32 function.

--- lib/IO/include/IO.h
+++ lib/IO/include/IO.h
@@ -3,3 +3,4 @@
 
 
 void print_bytes(const uint8_t *bytes, size_t n);
+void print_words32(const uint32_t *words, size_t n);
--- lib/IO/src/IO.c
+++ lib/IO/src/IO.c
@@ -19,3 +19,27 @@ void print_bytes(const uint8_t *bytes, size_t n)
     }
     printf("\n");
 }
+
+/**
+ * @brief Prints the supplied words as little-endian 32-bit integers.
+ * 
+ * @param words A pointer to the words to print
+ * @param n The number of words to print
+ */
+void print_words32(const uint32_t *words, size_t n)
+{
+    size_t row_length = 8;
+    size_t rows = (n + (row_length - 1)) / row_length;
+    size_t rem = rows * row_length - n;
+
+    for (size_t i = 0; i < n; i++) {
+        printf("%08x ", words[i]);
+        if ((i+1) % row_length == 0 && i != n-1) {
+            printf("\n");
+        }
+    }
+    for (size_t i = 0; i < rem; i++) {
+        printf("........ ");
+    }
+    printf("\n");
+}
