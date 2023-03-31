5ddbd1758a8b8698da7ea1b7335bf64b376c37c6
Daniel John <daniel.john.projects@gmail.com>
2023-03-31 21:13:36 +1100

    Formatting

--- cryptolib/publickey/DiffieHellman/DiffieHellman.py
+++ cryptolib/publickey/DiffieHellman/DiffieHellman.py
@@ -1,21 +1,22 @@
 """
 """
-from typing import Callable, NewType
+from typing import Callable, NewType, Tuple
 
 DH_Private_Key = NewType('DH_Private_Key', int)
-DH_Public_Key = tuple[int, int, int]
+DH_Public_Key = Tuple[int, int, int]  # Modulus p, base point g, g**x
+
 
 def DH_Factory(
-        p: int, 
-        g: int, 
-        number_generator: Callable[[], int], 
-        KDF: Callable[[int], bytes]
-    ) -> (Callable[[], tuple[DH_Private_Key, DH_Public_Key]], Callable[[DH_Private_Key, DH_Public_Key], bytes]):
+    p: int,
+    g: int,
+    number_generator: Callable[[], int],
+    KDF: Callable[[int], bytes]
+) -> Tuple[Callable[[], Tuple[DH_Private_Key, DH_Public_Key]], Callable[[DH_Private_Key, DH_Public_Key], bytes]]:
     """
     Given a parameter set creates the following oracles:
         - A key generation oracles that generate a public and private key.
         - An oracle that takes in a private key, and someone else's public key and derives a shared secret.
-    
+
     Args:
         p - The order of the group. This is normally prime.
         g - The base point of the group. This is normally a generator of the multiplicative group modulo p.
@@ -24,18 +25,20 @@ def DH_Factory(
     Returns:
         A key generation oracle and a shared secret oracle.
     """
-    def key_generation_oracle() -> (DH_Private_Key, DH_Public_Key):
+    def key_generation_oracle() -> Tuple[DH_Private_Key, DH_Public_Key]:
         private = DH_Private_Key(number_generator())
         pub = (p, g, pow(g, private, p))
         return private, pub
-    
+
     def key_agreement_oracle(private: DH_Private_Key, pub: DH_Public_Key) -> bytes:
-        p1, g1, pub = pub
+        p1, g1, pk = pub
         if p1 != p:
-            raise RuntimeError(f"Incompatible public modulus! My modulus is {p}, I was asked to form a shared secret with a modulus of {p1}.")
+            raise RuntimeError(
+                f"Incompatible public modulus! My modulus is {p}, I was asked to form a shared secret with a modulus of {p1}.")
         if g1 != g:
-            raise RuntimeError(f"Incompatible public base point! My base point is {g}, I was asked to form a shared secret with a base point of {g1}.")
-        shared_secret = pow(pub, private, p)
+            raise RuntimeError(
+                f"Incompatible public base point! My base point is {g}, I was asked to form a shared secret with a base point of {g1}.")
+        shared_secret = pow(pk, private, p)
         return KDF(shared_secret)
-    
+
     return key_generation_oracle, key_agreement_oracle
--- cryptolib/publickey/DiffieHellman/implementations.py
+++ cryptolib/publickey/DiffieHellman/implementations.py
@@ -2,7 +2,8 @@ import random
 import secrets
 from math import ceil
 from .DiffieHellman import DH_Factory
-from ...hashes.SHA1 import sha1digest
+from ...hashes.SHA2 import sha256digest
+
 
 def micro_DH():
     """
@@ -11,10 +12,13 @@ def micro_DH():
     rng = random.Random(12345)
     p = 37
     g = 5
-    return DH_Factory(p, g,
-        number_generator=lambda : rng.randrange(2, p),
-        KDF = lambda k: k.to_bytes(2, 'big')
+    return DH_Factory(
+        p,
+        g,
+        number_generator=lambda: rng.randrange(2, p),
+        KDF=lambda k: k.to_bytes(2, 'big')
     )
+
 
 def modp2048():
     """
@@ -26,14 +30,17 @@ def modp2048():
     """
     p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
     p_len = p.bit_length()//8
-    
+
     g = 2
-    
-    return DH_Factory(p, g,
-        number_generator=lambda : secrets.randbelow(p), 
-        KDF = lambda k: sha1digest(k.to_bytes(p_len, 'big'))
+
+    return DH_Factory(
+        p,
+        g,
+        number_generator=lambda: secrets.randbelow(p),
+        KDF=lambda k: sha256digest(k.to_bytes(p_len, 'big'))
     )
-    
+
+
 def export_grade():
     """
     This parameter set was shown to be exploitable in the famous Logjam attack.
@@ -41,11 +48,13 @@ def export_grade():
         https://dl.acm.org/doi/pdf/10.1145/2810103.2813707
     """
     p = 0x9fdb8b8a004544f0045f1737d0ba2e0b274cdf1a9f588218fb435316a16e374171fd19d8d8f37c39bf863fd60e3e300680a3030c6e4c3757d08f70e6aa87103
-    
+
     p_len = ceil(p.bit_length()/8)
-    
+
     g = 2
-    return DH_Factory(p, g,
-        number_generator=lambda : secrets.randbelow(p), 
-        KDF = lambda k: sha1digest(k.to_bytes(p_len, 'big'))
+    return DH_Factory(
+        p,
+        g,
+        number_generator=lambda: secrets.randbelow(p),
+        KDF=lambda k: sha256digest(k.to_bytes(p_len, 'big'))
     )
